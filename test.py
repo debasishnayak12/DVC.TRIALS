c47bfcc1a167642f4df59bb69b22afdb2f617385
debasishnayak12 <nayakdebasish7205@gmail.com>
2023-11-25 10:28:10 +0530

    fourth commit

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import pandas as pd
 
 data=[
     
-    {"name":"sunny","age":20,"city":"hopal"},
+    {"name":"sunny","age":20,"city":"bhopal"},
     {"name":"Krish","age":25,"city":"delhi"},
-    {"name":"debu","age":21,"city":"jajpur"}
+    {"name":"debu","age":21,"city":"jajpur"},
+    {"name":"vikash","age":23,"city":"Mumbai"}
      
 ]
 df=pd.DataFrame(data)
-df.to-csv("data/data.csv",index=False)
+df.to_csv("data/data.csv",index=False)
